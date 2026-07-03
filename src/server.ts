8dd396e532b2e2d777de08b332892bb77f0039dd
GabrielManfio <gabrielscarpin5@gmail.com>
2026-07-03 11:08:25 -0300

    Start my firts API minimal

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import fastify from 'fastify';
+
+const server = fastify({ logger: true });
+
+const teams = [
+  { id: 1, name: "Mercedes", base: "Brackley" },
+  { id: 2, name: "Red Bull Racing", base: "Milton Keynes" },
+  { id: 3, name: "Ferrari", base: "Maranello" }
+];
+
+const drivers = [
+  { id: 1, name: "Kimi Antonelli", team: "Mercedes" },
+  { id: 2, name: "Max Verstappen", team: "Red Bull Racing" },
+  { id: 3, name: "Charles Leclerc", team: "Ferrari" }
+];
+
+server.get("/teams", async (request, reply) => {
+  reply.type("application/json").code(200)
+  return teams;
+});
+
+server.get("/drivers", async (request, reply) => {
+  reply.type("application/json").code(200)
+  return drivers;
+});
+
+
+server.listen({port: 3000 }, () => {
+    console.log("Server init")
+});
